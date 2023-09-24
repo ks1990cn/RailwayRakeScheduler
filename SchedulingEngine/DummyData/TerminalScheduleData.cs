513c3a9abac2e569dd7fa40a6954aa59e332ef51
Tanmay Seth <kushal.seth2010@gmail.com>
2023-09-25 00:35:41 +0530

    Adding dummy data

--- SchedulingEngine/DummyData/TerminalScheduleData.cs
+++ SchedulingEngine/DummyData/TerminalScheduleData.cs
@@ -3,7 +3,6 @@
     public static class TerminalTrainScheduleData
     {
         public static Dictionary<int,List<(int, DateTime, DateTime, bool)>> TrainSchedulePerTerminal { get; set; }
-
         static TerminalTrainScheduleData()
         {
             TrainSchedulePerTerminal = GetTrainSchedulePerTerminal();
@@ -18,6 +17,31 @@
         /// <returns></returns>
         private static Dictionary<int, List<(int, DateTime, DateTime, bool)>> GetTrainSchedulePerTerminal()
         {
+            var schedulePerTerminal =new Dictionary<int, List<(int, DateTime, DateTime, bool)>>();
+
+            var terminals = new List<int>() { 6,1,3,5};
+
+            var startTimes = new List<DateTime>() {
+                
+                new DateTime(0, 0, 0, 2, 0, 0),
+                new DateTime(0,0,0,3,0,0),
+                new DateTime(0,0,0,16,0,0),
+                new DateTime(0,0,0,20,0,0)
+            };
+            foreach (var startTime in startTimes)
+            {
+                foreach (var terminal in terminals)
+                {
+                    var trainsOnTerminal = new List<(int, DateTime, DateTime, bool)>();
+
+                    Random random = new Random();
+
+                    var randomTrain = TrainDetails.Trains.First(a => a.TrainNumber == random.Next(1, 100));
+
+                    schedulePerTerminal.Add(terminal, trainsOnTerminal);
+                }
+            }
+           
             return new Dictionary<int, List<(int, DateTime, DateTime, bool)>>();
         }
     }
