2b8c030a2fab9b2ab8d7131f3d2b33007c9fd384
Tanmay Seth <kushal.seth2010@gmail.com>
2023-09-29 01:01:21 +0530

    Working on linear problem solving

--- RailwayRakeScheduler/Program.cs
+++ RailwayRakeScheduler/Program.cs
@@ -6,8 +6,45 @@ internal class Program
     private static async Task Main(string[] args)
     {
         int V = 11;
+
         PathFinder dijkstra = new PathFinder(V);
+
         SchedulerPathRunner schedulerPathRunner = new SchedulerPathRunner();
+        
+        CreateGraphData(dijkstra);
+        
+        var schedulingTrain = TrainDetails.Trains.First(a => a.TrainNumber == 0);
+
+        List<int> terminalsOnWhichSchedulingImpossible = new List<int>();
+
+        Dictionary<int, DateTime> SchedulePerTerminal = new Dictionary<int, DateTime>();
+
+        var existingTrainSchedulePerTerminal = TerminalTrainScheduleData.TrainSchedulePerTerminal;
+
+        do
+        {
+            // TODO: Here we can fetch any existing shortest path from Database
+
+            dijkstra.ScheduledShortestPath(6, 5);
+
+            var shortestPathFound = dijkstra.ResultPath;
+
+            schedulerPathRunner.TryToRunOnPath(shortestPathFound, terminalsOnWhichSchedulingImpossible, ref SchedulePerTerminal);
+
+            if (terminalsOnWhichSchedulingImpossible.Count() == 0)
+            {
+                foreach (var schedules in SchedulePerTerminal)
+                {
+                    Console.WriteLine(schedules.Key + " " + schedules.Value);
+                }
+            }
+
+        } while (terminalsOnWhichSchedulingImpossible.Any());
+
+    }
+
+    private static void CreateGraphData(PathFinder dijkstra)
+    {
         dijkstra.AddEdge(6, 1, 600);
         dijkstra.AddEdge(1, 2, 700);
         dijkstra.AddEdge(2, 9, 200);
@@ -22,34 +59,5 @@ internal class Program
         dijkstra.AddEdge(1, 3, 450);
         dijkstra.AddEdge(3, 5, 300);
         dijkstra.AddEdge(4, 5, 200);
-        
-        var schedulingTrain = TrainDetails.Trains.First(a => a.TrainNumber == 6);
-
-        List<int> terminalsOnWhichSchedulingImpossible = new List<int>();
-
-        Dictionary<int,DateTime> SchedulePerTerminal = new Dictionary<int,DateTime>();
-
-        var trainSchedulePerTerminal = TerminalTrainScheduleData.TrainSchedulePerTerminal;
-
-        do
-        {
-            // TODO: Here we can fetch any existing shortest path from Database
-
-            dijkstra.ScheduledShortestPath(6, 5);
-
-            var shortestPathFound = dijkstra.ResultPath;
-
-            schedulerPathRunner.TryToRunOnPath(shortestPathFound,terminalsOnWhichSchedulingImpossible, ref SchedulePerTerminal);
-
-            if(terminalsOnWhichSchedulingImpossible.Count() == 0)
-            {
-                foreach (var schedules in SchedulePerTerminal)
-                {
-                    Console.WriteLine(schedules.Key + " " + schedules.Value);
-                }
-            }
-
-        } while (terminalsOnWhichSchedulingImpossible.Any());
-        
     }
 }
--- SchedulingEngine/Scheduler/SchedulerPathRunner.cs
+++ SchedulingEngine/Scheduler/SchedulerPathRunner.cs
@@ -1,4 +1,5 @@
-﻿using SchedulingEngine.DummyData;
+﻿using Google.OrTools.LinearSolver;
+using static Google.OrTools.LinearSolver.Solver;
 
 namespace SchedulingEngine.Scheduler
 {
@@ -6,16 +7,53 @@ namespace SchedulingEngine.Scheduler
     {
         public SchedulerPathRunner()
         {
-                
+
         }
         /// <summary>
         /// This method will be responsible to try to run on given path considering other trains on path and schedule of terminals.
         /// If return false then algorithm will try to find any other path
         /// </summary>
         /// <returns></returns>
-        public bool TryToRunOnPath(List<int> shortestPath, List<int> terminalsOnWhichSchedulingImpossible, ref Dictionary<int, DateTime> schedulePerTerminal)
+        public void TryToRunOnPath(List<int> shortestPath, List<int> terminalsOnWhichSchedulingImpossible, ref Dictionary<int, DateTime> schedulePerTerminal)
         {
-            return true;
+           
+        }
+
+        public static DateTime? SolveForOptimalDepartureTimeFromSource(DateTime arrivalTimeDestination, DateTime[] timeRanges, DateTime specifiedDepartureTime)
+        {
+            // Create a new LP model.
+            Solver solver = Solver.CreateSolver("DepartureTimeSolver");
+
+            // Define the decision variables.
+            Variable departureTimeFromSourceVariable = solver.MakeNumVar(0, Double.PositiveInfinity, "departureTimeFromSource");
+            Variable arrivalTimeDestinationVariable = solver.MakeNumVar(arrivalTimeDestination.Ticks, arrivalTimeDestination.Ticks, "arrivalTimeDestination");
+
+            // Define the constraints.
+            solver.Add(arrivalTimeDestinationVariable - departureTimeFromSourceVariable == 10);
+
+            foreach (DateTime timeRange in timeRanges)
+            {
+                Variable timeRangeVariable = solver.MakeNumVar(timeRange.Ticks, timeRange.Ticks, "timeRange");
+                
+                //solver.Add(timeRangeVariable - arrivalTimeDestinationVariable < 0);
+                //solver.Add(arrivalTimeDestination < timeRange.AddTicks(1));
+            }
+
+            //solver.Add(departureTimeFromSourceVariable > specifiedDepartureTime.Ticks);
+
+            // Define the objective function.
+            solver.Minimize(departureTimeFromSourceVariable - specifiedDepartureTime.Ticks);
+
+            // Solve the LP model.
+            Solver.ResultStatus solution = solver.Solve();
+
+            if (solution == Solver.ResultStatus.OPTIMAL)
+            {
+                
+            }
+            return null;
+            // Get the optimal value for departureTimeFromSource.
+            //return new DateTime(solver.Objective().Value());
         }
     }
 }
